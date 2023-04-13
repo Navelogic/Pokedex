8a7bb9945dd03bdb6c6fb4e319e8e5237194837b
Arian Weslley <weslleysantoszbd@gmail.com>
2023-04-13 16:51:01 -0300

    response

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,9 @@ let limit = 10;
 let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
 fetch(url)
-    .then(function (response) {
-    console.log(response);
-    })
-    .catch(function (error) {
-    console.log(error);
-    })
-    .finally(function () {
-    console.log('Request completed');
+    .then((response)=> response.json())
+    .then((data)=> {
+        for (let i = 0; i < data.results.length; i++) {
+            console.log(data.results[i].name);
+        }
     });
